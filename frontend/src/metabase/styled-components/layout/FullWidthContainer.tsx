2728c4706b5f1b9ec94e89d5e783dcf619d4a1f0
Uladzimir Havenchyk <uladzimir.dev@gmail.com>
2025-02-17 23:55:30 +0300

    use named function

--- frontend/src/metabase/styled-components/layout/FullWidthContainer.tsx
+++ frontend/src/metabase/styled-components/layout/FullWidthContainer.tsx
@@ -4,11 +4,10 @@ import { Box, type BoxProps } from "metabase/ui";
 
 import S from "./FullWidthContainer.module.css";
 
-// eslint-disable-next-line react/display-name
 export const FullWidthContainer = forwardRef<
   HTMLDivElement,
   BoxProps & { children?: React.ReactNode }
->((props, ref) => {
+>(function FullWidthContainer(props, ref) {
   return (
     <Box
       ref={ref}
